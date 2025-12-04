d7c75ee49d6c241a7ba05a7adb37122002e1452b
Administrator <Administrator@DYNCESS>
2025-12-04 14:39:58 +0530

    naa ray gi tarong

--- School-Clinic/Form1.Designer.cs
+++ School-Clinic/Form1.Designer.cs
@@ -38,10 +38,12 @@
             // pictureBox1
             // 
             pictureBox1.BackgroundImage = Properties.Resources.DMC_CLINIC1;
+            pictureBox1.BackgroundImageLayout = ImageLayout.Zoom;
             pictureBox1.Dock = DockStyle.Fill;
             pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Margin = new Padding(4, 5, 4, 5);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(900, 632);
+            pictureBox1.Size = new Size(1275, 897);
             pictureBox1.TabIndex = 0;
             pictureBox1.TabStop = false;
             pictureBox1.Click += pictureBox1_Click;
@@ -50,18 +52,20 @@
             // 
             textBox1.BackColor = Color.White;
             textBox1.BorderStyle = BorderStyle.None;
-            textBox1.Location = new Point(143, 313);
+            textBox1.Location = new Point(204, 442);
+            textBox1.Margin = new Padding(4, 5, 4, 5);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(218, 16);
+            textBox1.Size = new Size(311, 24);
             textBox1.TabIndex = 1;
             textBox1.TextChanged += textBox1_TextChanged;
             // 
             // textBox2
             // 
             textBox2.BorderStyle = BorderStyle.None;
-            textBox2.Location = new Point(143, 364);
+            textBox2.Location = new Point(204, 513);
+            textBox2.Margin = new Padding(4, 5, 4, 5);
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(218, 16);
+            textBox2.Size = new Size(311, 24);
             textBox2.TabIndex = 2;
             textBox2.TextChanged += textBox2_TextChanged;
             // 
@@ -70,23 +74,25 @@
             button1.BackColor = Color.FromArgb(51, 111, 63);
             button1.FlatStyle = FlatStyle.Flat;
             button1.ForeColor = Color.Transparent;
-            button1.Location = new Point(165, 438);
+            button1.Location = new Point(251, 599);
+            button1.Margin = new Padding(4, 5, 4, 5);
             button1.Name = "button1";
-            button1.Size = new Size(125, 41);
+            button1.Size = new Size(179, 68);
             button1.TabIndex = 3;
             button1.Text = "LOGIN";
             button1.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(900, 632);
+            ClientSize = new Size(1275, 897);
             Controls.Add(button1);
             Controls.Add(textBox2);
             Controls.Add(textBox1);
             Controls.Add(pictureBox1);
             FormBorderStyle = FormBorderStyle.FixedSingle;
+            Margin = new Padding(4, 5, 4, 5);
             Name = "Form1";
             Text = "LOGIN";
             Load += Form1_Load;
